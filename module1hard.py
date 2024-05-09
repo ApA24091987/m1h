6d8d78d2659574df56d63fd7f0708919a4446f42
ApA24091987 <168029434+ApA24091987@users.noreply.github.com>
2024-05-09 17:44:09 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -2,8 +2,9 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 a_grades = {}
 print(a_grades)
-s_students=sorted(students)
+s_students = sorted(students)
 print(s_students)
-for i, student in enumerate(s_students): a_grade = sum(grades[i]) / len(grades[i])
-a_grades[student] = a_grade
+for i, student in enumerate(s_students):
+    a_grade = sum(grades[i]) / len(grades[i])
+    a_grades[student] = a_grade
 print(a_grades)
